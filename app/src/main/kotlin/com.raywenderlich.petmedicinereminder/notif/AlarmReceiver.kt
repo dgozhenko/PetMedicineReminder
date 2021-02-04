46fc1d00effa39752372956b845be130161cf38d
daniil.gozhenko <daniil.gozhenko@gmail.com>
2021-02-04 15:30:59 +0200

    Create broadcast receiver for alarm

--- app/src/main/kotlin/com.raywenderlich.petmedicinereminder/notif/AlarmReceiver.kt
+++ app/src/main/kotlin/com.raywenderlich.petmedicinereminder/notif/AlarmReceiver.kt
@@ -3,10 +3,23 @@ package com.raywenderlich.petmedicinereminder.notif
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.raywenderlich.petmedicinereminder.R
+import com.raywenderlich.petmedicinereminder.data.DataUtils
+import com.raywenderlich.petmedicinereminder.reminder.ReminderDialog
 
 class AlarmReceiver: BroadcastReceiver(){
 
   override fun onReceive(context: Context?, intent: Intent?) {
-
+    if (context != null && intent != null && intent.action != null) {
+      if (intent.action!!.equals(context.getString(R.string.action_notify_administer_medication)
+                      , ignoreCase = true)) {
+        if (intent.extras != null) {
+          val reminderData = DataUtils.getReminderById(intent.extras!!.getInt(ReminderDialog.KEY_ID))
+          if (reminderData != null) {
+            NotificationHelper.createNotificationForPet(context, reminderData)
+          }
+        }
+      }
+    }
   }
 }
