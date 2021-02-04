bfc73317fcdcb0381b6946ad9cddf88bdf77d745
daniil.gozhenko <daniil.gozhenko@gmail.com>
2021-02-04 14:20:47 +0200

    Notify Notification Manager

--- app/src/main/kotlin/com.raywenderlich.petmedicinereminder/notif/NotificationHelper.kt
+++ app/src/main/kotlin/com.raywenderlich.petmedicinereminder/notif/NotificationHelper.kt
@@ -94,7 +94,10 @@ object NotificationHelper {
       intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
       val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0 )
       setContentIntent(pendingIntent)
+
     }
+    val notificationManager = NotificationManagerCompat.from(context)
+    notificationManager.notify(1001, notificationBuilder.build())
   }
 
   /**
